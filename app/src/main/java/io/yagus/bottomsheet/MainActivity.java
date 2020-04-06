db967caa7daa405615cc91c6eeaa1e197f9b29a6
KJH <jinhungkim@hanmail.net>
2020-04-06 10:58:58 +0900

    NEW

--- app/src/main/java/io/yagus/bottomsheet/MainActivity.java
+++ app/src/main/java/io/yagus/bottomsheet/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
@@ -19,8 +20,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+                final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
                 bottomSheetDialog.setContentView(R.layout.bottom_sheet);
+
+                Button button_dialog = bottomSheetDialog.findViewById(R.id.button_dialog);
+                button_dialog.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+
                 bottomSheetDialog.show();
             }
         });
